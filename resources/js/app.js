1a4d8370e0db59908eaee000344db1604abaac7c
Shaun Parkison <shparkison@gmail.com>
2026-04-13 13:38:02 -0600

    chore: Fix flyout menu hidden when using collapsed nav on desktop
    Disable overlay scrollbar since it's not working as intended

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,49 +1,6 @@
 // Import styles (Support HMR)
 import '../css/app.css'
 
-// OverlayScrollbars
-import { OverlayScrollbars } from 'overlayscrollbars'
-
-const OS_OPTIONS = {
-    scrollbars: {
-        theme: 'os-theme-app',
-        autoHide: 'scroll',
-        autoHideDelay: 800,
-        clickScroll: true,
-    },
-}
-
-const OS_SELECTORS = [
-    // 'body',
-    '.fi-sidebar-nav',
-].join(', ')
-
-const initScrollbars = () => {
-    document.querySelectorAll(OS_SELECTORS).forEach(el => {
-        if (!OverlayScrollbars(el)) {
-            OverlayScrollbars(el, OS_OPTIONS)
-        }
-    })
-}
-
-let osDebounce
-const debouncedInit = () => {
-    clearTimeout(osDebounce)
-    osDebounce = setTimeout(initScrollbars, 120)
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    initScrollbars()
-    // Catch Livewire-rendered elements (tables, modals, etc.)
-    new MutationObserver(debouncedInit).observe(document.body, {
-        childList: true,
-        subtree: true,
-    })
-})
-document.addEventListener('livewire:navigated', initScrollbars)
-// Fires after every Livewire server round-trip (table loads, filters, pagination)
-document.addEventListener('livewire:commit', debouncedInit)
-
 // Enable websockets
 import './echo'
 
@@ -56,6 +13,7 @@ window.Hls = Hls
 window.mpegts = mpegts
 
 // Vendor
+// import './vendor/overlay-scrollbar'
 import './vendor/qrcode'
 import './vendor/epg-viewer'
 import './vendor/stream-viewer'
